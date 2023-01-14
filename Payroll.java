4e6d7f679e7271d562ae23c1615a1ccd923485a3
ShantaBhuiya <110441388+ShantaBhuiya@users.noreply.github.com>
2023-01-14 16:25:27 -0500

    Add files via upload

---
+++ Payroll.java
@@ -0,0 +1,14 @@
+package com.syntexpro.BasicJava;
+
+public class Payroll {
+    public static void main(String[] args) {
+        int hour = 40;
+        double payRate = 25;
+
+        double grosspay = hour*payRate;
+
+        System.out.println("Your gross pay is $"+grosspay);
+
+
+    }
+}
